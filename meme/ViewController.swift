3a4d1bc25fca90d0ad53e04b7ae16e6d6cae97da
Satoshi Ebisawa <e.satoshi@gmail.com>
2015-07-25 10:08:45 +0900

    Setup MEMELib

---
+++ meme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meme
+//
+//  Created by Satoshi Ebisawa on 2015/07/25.
+//  Copyright (c) 2015年 Satoshi Ebisawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ meme/meme-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  meme-Bridging-Header.h
+//  meme
+//
+//  Created by Satoshi Ebisawa on 2015/07/25.
+//  Copyright (c) 2015年 Satoshi Ebisawa. All rights reserved.
+//
+
+#ifndef meme_meme_Bridging_Header_h
+#define meme_meme_Bridging_Header_h
+
+#import <MEMELib/MEMELib.h>
+
+#endif
